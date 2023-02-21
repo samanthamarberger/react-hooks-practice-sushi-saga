630eb5a4a4df53257152b50e8412c99c187e9715
samanthamarberger <slatham04@gmail.com>
2023-02-21 13:39:04 -0700

    Working on conveyer belt

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
 const API = "http://localhost:3001/sushis";
 
 function App() {
+
+  const [sushis, setSushis] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/sushis")
+    .then ((r) => r.json())
+    .then((sushis) => setSushis(sushis));
+  }, [])
+
+  //console.log(sushis);
+
   return (
     <div className="app">
-      <SushiContainer />
+      <SushiContainer sushis={sushis}/>
       <Table />
     </div>
   );
--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import MoreButton from "./MoreButton";
+import Sushi from "./Sushi";
 
-function SushiContainer(props) {
+function SushiContainer({ sushis }) {
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  sushis.map((sushi) => console.log(sushi))
+
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
+      {sushis.map((sushi) => <Sushi key={sushi.id} name={sushi.name} price={sushi.price} img={sushi.img_url}/>)}
       <MoreButton />
     </div>
   );
